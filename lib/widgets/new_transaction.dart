29363e79d34ce0ff295cc053cb5a1367e6e8aac5
GhaidaAlomran <439200697@student.ksu.edu.sa>
2021-10-07 21:55:46 +0300

    git

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -31,10 +31,10 @@ class _NewTransactionState extends State<NewTransaction> {
 
     Navigator.of(context).pop();
   } //علشان يربط الفنشكن الي جتنا من الكلاس اللي فوق مع هذا الكلاس
-
+//gg
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
+    return SingleChildScrollViewgit remote add origin https://github.com/GhaidaAlomran/test55.git(
       child: Card(
         elevation: 5,
         child: Container(
